b12a367f91d94d1c2a5effe4e3ff7032912c0a5a
Soudeh Jahanshahi <soudeh@degas.thphy.uni-duesseldorf.de>
2015-11-13 17:50:56 +0100

    Statistics

--- statistics.cxx
+++ statistics.cxx
@@ -3,19 +3,40 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
 
+void producer(double* p,const int N){
+  for(int i=0; i<N; i++){
+ p[i]=double(rand())/RAND_MAX;  //since rand() & RAND_MAX are both integer.
+  }
+}
+void statistics(double* p,double& mean, double& var, const int N){
+  mean=0;
+ for(int i=0; i<N; i++){
+   mean+=p[i];
+ }
+ mean=mean/N;
+ 
+ var=0;
+ for(int i=0; i<N; i++){
+  var+=(p[i]-mean)*(p[i]-mean);
+ }
+ var=var/N;
+}
 int main(){
+  srand(time(NULL));
    const int N = 100;
    double p[N];
    double mean, var;
 
-   // Some lines here....
+   producer(p,N);
+   statistics(p,mean,var,N);
 
    cout << "Mean = " << mean << endl;
-   cout << "Variance = " << var << endl;
+   cout << "Variance = " << var << endl;  
+   //To make sure that there are really random numbers, use:
+   /*for(int i=0; i<N; i++){
+   cout<<i<<"\t"<<p[i]<<endl;
+   }*/
 
    return 0;
 }
